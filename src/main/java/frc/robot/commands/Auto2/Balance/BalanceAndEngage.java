8538cacf300d39757332ae3112fd0351e312e8e2
Me-Smrt <code_t@krummhere.com>
2023-03-16 15:41:44 -0400

    3-16-2023

--- src/main/java/frc/robot/commands/Auto2/Balance/BalanceAndEngage.java
+++ src/main/java/frc/robot/commands/Auto2/Balance/BalanceAndEngage.java
@@ -27,8 +27,21 @@ public class BalanceAndEngage extends CommandBase{
 
     @Override
     public void execute() {
+        double position = swerveDrive.getPosition();
+        double roll = m_navx_table.getEntry("navx_filtered_roll").getDouble(0.0);
+        m_count += 1;
+        if(m_count % 100 == 0){
+            if(roll > 2.0) stopPosition -= 0.1;
+            else if(roll < -2.0) stopPosition+= 0.1;
+        }
         
-        
+        double power = pid.calculate(position - stopPosition);
+
+        power = Math.min(Math.max(power,-m_max_power),m_max_power); // limit the speed
+
+        System.out.printf("set_pos:%.3f  pos:%.3f  angle:%.3f  power:%.3f\n", stopPosition, position, roll, power);
+
+        swerveDrive.setChassisSpeeds(0, 0, 0);
     }
 
     @Override
